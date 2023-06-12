7dd7eac4e776cd42781c336f3071b95d74ea14cb
SleepyMorfeusz <Sleepymorfeusz@gmail.com>
2023-06-12 14:01:54 +0200

    Add logo img and sponsors section, add script to toggle divs, style sponsors section

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+`use strict`;
+const dolina = document.querySelector(`.dolina-text-box`);
+const josera = document.querySelector(`.josera-text-box`);
+const logoDolina = document.querySelector(`.dolina-logo`);
+const logoJosera = document.querySelector(`.josera-logo`);
+logoDolina.addEventListener("click", function () {
+  dolina.classList.remove(`hidden`);
+  josera.classList.add(`hidden`);
+});
 
+logoJosera.addEventListener("click", function () {
+  dolina.classList.add(`hidden`);
+  josera.classList.remove(`hidden`);
+});
